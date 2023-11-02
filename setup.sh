9fdbbfab5cf67de107f6cf965fc2656b9a69157e
Ayushmaan Padhi <ayushmaanpadhi2805@gmail.com>
2023-11-02 15:34:28 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -144,7 +144,7 @@ read -r -p "Do you want to install TLP (and remove Gnome Power Profiles Daemon)?
 if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     echo ""
     sudo pacman -Rscn --noconfirm power-profiles-daemon
-    sudo pacman -Syu --needed --noconfirm tlp tlp-rdw gnome-power-manager
+    sudo pacman -Syu --needed --noconfirm tlp tlp-rdw smartmontools ethtool gnome-power-manager
     sudo systemctl enable tlp.service
     sudo systemctl enable NetworkManager-dispatcher.service
     sudo systemctl mask systemd-rfkill.service systemd-rfkill.socket
