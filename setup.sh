001ae44e079f946f16cf92a0e9c097c32ea17162
Ayushmaan Padhi <76616749+ayu2805@users.noreply.github.com>
2024-07-17 21:48:38 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -49,6 +49,7 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
     sudo pacman-key --lsign-key 3056513887B78AEB
     sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
     sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+    echo -e "[Chaotic-AUR]\nInclude = /etc/pacman.d/chaotic-mirrorlist" | sudo tee -a /etc/pacman.conf
 
     if [ "$(pactree -r yay-bin)" || "$(pactree -r yay)"]; then
         echo ""
